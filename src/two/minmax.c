605c1745631fa595384727fe906cd09d4b56d96c
Hiccstrid2019 <hiccup.colin@yandex.ru>
2023-03-01 21:07:37 +0300

    'two'

---
+++ src/two/minmax.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void maxmin(int prob1, int prob2, int prob3, int *max, int *min);
+
+int main()
+{
+    double x, y, z;
+    if (scanf("%lf", &x) == 1 && scanf("%lf", &y) == 1 && scanf("%lf", &z) == 1)
+    {
+        if ((int)x != x || (int)y != y || (int)z != z)
+        {
+            printf("n/a\n");
+        }
+        else
+        {
+            int max, min;
+            maxmin(x, y, z, &max, &min);
+            printf("%d %d\n", max, min);
+        }
+    }
+    else
+    {
+        printf("n/a\n");
+    }
+
+    return 0;
+}
+
+void maxmin(int prob1, int prob2, int prob3, int *max, int *min)
+{
+    *max = *min = prob1;
+    if (prob2 > *max)
+        *max = prob2;
+    if (prob2 < *min)
+        *min = prob2;
+
+    if (prob3 > *max)
+        *max = prob3;
+    if (prob3 < *min)
+        *min = prob3;
+}
